ce105736686027fd9f9cbae7627d8fd54d956c53
Vikas Srivastava <er.vikassri@gmail.com>
2020-08-19 21:34:27 +0530

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,7 @@
 source ./env_files/env.sh
 # spark-submit --jars mssql-jdbc-8.4.0.jre8.jar sqlServerToHive.py <config> <database>
-spark-submit --jars mssql-jdbc-8.4.0.jre8.jar sqlServerToHive.py configs/config.ini <database>
+for database in `cat $1`
+do
+  spark-submit --jars mssql-jdbc-8.4.0.jre8.jar sqlServerToHive.py configs/config.ini $database
+  sleep 5
+done
